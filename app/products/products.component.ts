da0a136f5e2df801950b87fcdc3359dc33460810
anguyen2002 <79826325+anguyen2002@users.noreply.github.com>
2025-10-08 16:00:52 -0600

    Add files via upload

--- app/products/products.component.ts
+++ app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../service/product.service';
 import { errorContext } from 'rxjs/internal/util/errorContext';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -11,8 +12,15 @@ import { errorContext } from 'rxjs/internal/util/errorContext';
 })
 export class ProductsComponent implements OnInit{
   productsList?:any = [];
-  errorMessage = "";
-  
+  errorMessage:string = "";
+  addingProduct:boolean = false;
+
+  newTitle:string = '';
+  newDescription:string = '';
+  newCategory:string = '';
+
+  editingIndex: number | null = null;
+
   constructor(private productService: ProductService){
 
   }
@@ -28,6 +36,47 @@ export class ProductsComponent implements OnInit{
         this.errorMessage = 'Error: '+err.message;
       },
       complete: () => console.log('Completed')
-  });
+    });
+  }
+
+  addProduct() {
+    this.addingProduct = true;
+  }
+
+  onSubmitAddProduct() {
+    if(!this.newTitle || !this.newDescription || !this.newCategory) {
+      alert('You must fill out all fields');
+    }
+
+    const newProduct = {
+      title: this.newTitle,
+      description: this.newDescription,
+      category: this.newCategory
+    }
+
+    this.productsList.push(newProduct);
+  }
+
+  onCancelAddProduct() {
+    this.addingProduct = false;
+  }
+
+  onUpdate(productIndex:number) {
+    this.editingIndex = productIndex;
+    console.log(this.editingIndex);
+  }
+
+  updateProduct(productIndex:number) {
+    if(!this.newTitle || !this.newDescription || !this.newCategory) {
+      alert('You must fill out all fields');
+    }
+    const product = this.productsList[productIndex];
+    product.title = this.newTitle;
+    product.description = this.newDescription;
+    product.category = this.newCategory;
+  }
+
+  closeEditing() {
+    this.editingIndex = null;
   }
 }
